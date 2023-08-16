246cec8869e1a2added4696a53a8a47d64052711
Emily <emilyonishi93@gmail.com>
2023-08-16 09:58:30 -0700

    add logic 2 compare sides

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+function calculation(event) {
+    event.preventDefault();
+    const num1 = parseInt(document.querySelector("side1").value);
+    const num2 = parseInt(document.querySelector("side2").value);
+    const num3 = parseInt(document.querySelector("side3").value);
+
+    let result;
+if (num1 === num2 && num1 === num3) {
+    result 
+    } else if (num1 === num2 || num1 === num 3 || num2 === num3) {
+        result
+    } else if (num1 !=== num2 && num1 !=== num3) && num2 !=== num3 {
+        result
+    } else {
+
+    }
+}

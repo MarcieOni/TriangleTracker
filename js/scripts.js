69a68b309f49b183c7ecfebe9f33a54f6f9eb09d
Emily <emilyonishi93@gmail.com>
2023-08-16 10:15:44 -0700

    add more js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 function calculation(event) {
     event.preventDefault();
+    const form = document.querySelector("form");
     const num1 = parseInt(document.querySelector("side1").value);
     const num2 = parseInt(document.querySelector("side2").value);
     const num3 = parseInt(document.querySelector("side3").value);
+
+    form.addEventListener("submit", function() {
+        
+    });
 
     let result;
     if (num1 === num2 && num1 === num3) {
